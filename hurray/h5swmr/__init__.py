d8438664f97f7dcc29917630189416f8af187493
rg <remo.goetschi@meteotest.ch>
2017-01-30 23:11:13 +0100

    init website

--- hurray/h5swmr/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-A drop-in replacement for the h5py library. Allows "single write multiple
-read" (SWMR) access to hdf5 files.
-"""
-
-from .api import File, Node, Dataset, Group
-
-__all__ = ["File", "Node", "Dataset", "Group"]
--- tests/__main__.py
+++
@@ -1,9 +0,0 @@
-"""
-Runs all tests
-"""
-
-import unittest
-
-from . import full_suite
-
-unittest.TextTestRunner().run(full_suite())
