c87bc52fecb7b06d84f441ec006c364c3e58ff1f
rg <remo.goetschi@meteotest.ch>
2017-01-31 02:36:52 +0100

    github page

---
+++ hurray/__main__.py
@@ -0,0 +1,145 @@
+# Copyright (c) 2016, Meteotest
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of Meteotest nor the
+#      names of its contributors may be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import absolute_import
+
+import struct
+import logging
+from concurrent.futures import ProcessPoolExecutor
+
+import msgpack
+
+from hurray.server import gen
+from hurray.server.ioloop import IOLoop
+from hurray.server.iostream import StreamClosedError
+from hurray.server.log import app_log
+from hurray.server.netutil import bind_unix_socket, bind_sockets
+from hurray.server.options import define, options
+from hurray.server.tcpserver import TCPServer
+from hurray.status_codes import INTERNAL_SERVER_ERROR
+from hurray.msgpack_ext import decode, encode
+from hurray.request_handler import handle_request
+from hurray.h5swmr.sync import clear_locks
+
+MSG_LEN = 4
+PROTOCOL_VER = 1
+
+# command line arguments
+define("host", default='localhost', group='application',
+       help="IP address or hostname")
+define("port", default=2222, group='application',
+       help="TCP port to listen on (0 = do not listen on a port)")
+define("socket", default=None, group='application',
+       help="Unix socket path")
+define("processes", default=0, group='application',
+       help="Number of sub-processes (0 = detect the number of cores available"
+       " on this machine)")
+define("debug", default=0, group='application',
+       help="Write debug information to stdout?")
+define("redis_host", default="localhost", group='application',
+       help="Redis server host name")
+define("redis_port", default=6379, group='application',
+       help="Redis server port")
+define("redis_db", default=0, group='application',
+       help="Redis server db number")
+
+
+class HurrayServer(TCPServer):
+
+    @gen.coroutine
+    def handle_stream(self, stream, address):
+        pool = ProcessPoolExecutor(max_workers=1)
+        while True:
+            try:
+                # read protocol version
+                protocol_ver = yield stream.read_bytes(MSG_LEN)
+                protocol_ver = struct.unpack('>I', protocol_ver)[0]
+
+                # Read message length (4 bytes) and unpack it into an integer
+                raw_msg_length = yield stream.read_bytes(MSG_LEN)
+                msg_length = struct.unpack('>I', raw_msg_length)[0]
+
+                app_log.debug("Handle request (Protocol: v%d, Msg size: %d)",
+                              protocol_ver, msg_length)
+
+                data = yield stream.read_bytes(msg_length)
+                msg = msgpack.unpackb(data, object_hook=decode,
+                                      use_list=False, encoding='utf-8')
+
+                try:
+                    fut = pool.submit(handle_request, msg)
+                    response = yield fut
+                except Exception:
+                    app_log.exception('Error in subprocess')
+                    response = msgpack.packb({
+                        'status': INTERNAL_SERVER_ERROR,
+                    }, default=encode)
+
+                yield stream.write(struct.pack('>I', PROTOCOL_VER))
+
+                # Prefix each message with a 4-byte length (network byte order)
+                yield stream.write(struct.pack('>I', len(response)))
+
+                yield stream.write(response)
+            except StreamClosedError:
+                app_log.info("Lost client at host %s", address)
+                break
+            except Exception:
+                app_log.exception('Error while handling client connection')
+        pool.shutdown()
+
+
+def main():
+    options.parse_command_line()
+
+    debug = bool(options.debug)
+    if debug:
+        app_log.setLevel(logging.DEBUG)
+        app_log.debug("debug mode")
+
+    server = HurrayServer()
+
+    sockets = []
+
+    if options.port != 0:
+        sockets = bind_sockets(options.port, options.host)
+        app_log.info("Listening on %s:%d", options.host, options.port)
+
+    if options.socket:
+        app_log.info("Listening on %s", options.socket)
+        sockets.append(bind_unix_socket(options.socket))
+
+    if len(sockets) < 1:
+        app_log.error('Define a socket and/or a port > 0')
+        return
+
+    clear_locks()
+    server.start(options.processes)
+    server.add_sockets(sockets)
+    IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
---
+++ hurray/msgpack_ext.py
@@ -0,0 +1,103 @@
+# Copyright (c) 2016, Meteotest
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of Meteotest nor the
+#      names of its contributors may be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+Msgpack encoders and decoders for numpy "objects" (arrays, types,
+scalars) and slices.
+"""
+
+from inspect import isclass
+
+import numpy as np
+from numpy.lib.format import header_data_from_array_1_0
+
+from .h5swmr import Group, Dataset
+from hurray.protocol import (RESPONSE_NODE_TYPE, NODE_TYPE_GROUP,
+                             NODE_TYPE_DATASET, RESPONSE_NODE_SHAPE,
+                             RESPONSE_NODE_DTYPE, RESPONSE_NODE_PATH)
+
+
+def encode(obj):
+    """
+    Encode numpy arrays and slices
+    :param obj: object to serialize
+    :return: dictionary with encoded array or slice
+    """
+    if isinstance(obj, np.ndarray):
+        arr = header_data_from_array_1_0(obj)
+        arr['arraydata'] = obj.tostring()
+        arr['__ndarray__'] = True
+        return arr
+    elif isinstance(obj, slice):
+        return {
+            '__slice__': (obj.start, obj.stop, obj.step)
+        }
+    elif isclass(obj) and issubclass(obj, np.number):
+        # make sure numpy type classes such as np.float64 (used, e.g., as dtype
+        # arguments) are serialized to strings
+        return obj().dtype.name
+    elif isinstance(obj, np.dtype):
+        return obj.name
+    elif isinstance(obj, np.number):
+        # convert to Python scalar
+        return np.asscalar(obj)
+    elif isinstance(obj, Group):
+        # TODO include attrs?
+        data = {
+            RESPONSE_NODE_TYPE: NODE_TYPE_GROUP,
+            RESPONSE_NODE_PATH: obj.path,
+        }
+        return data
+    elif isinstance(obj, Dataset):
+        # TODO include attrs?
+        data = {
+            RESPONSE_NODE_TYPE: NODE_TYPE_DATASET,
+            RESPONSE_NODE_PATH: obj.path,
+            RESPONSE_NODE_SHAPE: obj.shape,
+            RESPONSE_NODE_DTYPE: obj.dtype,
+        }
+        return data
+
+    return obj
+
+
+def decode(obj):
+    """
+    Decode numpy arrays and slices
+    :param obj: object to decode
+    :return: numpy array or slice
+    """
+
+    if '__ndarray__' in obj:
+        arr = np.fromstring(obj['arraydata'], dtype=np.dtype(obj['descr']))
+        shape = obj['shape']
+        arr.shape = shape
+        if obj['fortran_order']:
+            arr.shape = shape[::-1]
+            arr = arr.transpose()
+        return arr
+    elif '__slice__' in obj:
+        return slice(*obj['__slice__'])
+
+    return obj
