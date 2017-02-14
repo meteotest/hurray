45e890669dff27bebf5253a56e392134da50c027
Remo Goetschi <remo.goetschi@meteotest.ch>
2017-02-14 14:43:36 +0100

    generate logo without margin

--- logo.py
+++ logo.py
@@ -4,6 +4,7 @@ Create hurray logo
 
 import numpy as np
 from scipy.ndimage import zoom
+import matplotlib as mpl
 from matplotlib import pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -13,7 +14,7 @@ def create_logo():
     data = np.array([
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0],
         [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
         [0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0],
         [0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0],
@@ -28,38 +29,34 @@ def create_logo():
 
     data = zoom(data, zoom=1.6, order=5, mode="constant")
     cmap = "YlGnBu"
-    # fig = plt.imshow(data, cmap=cmap)
-    # fig = plt.pcolor(data[::-1], edgecolors='#888888', linewidths=0.4)
-    # fig.axes.get_xaxis().set_visible(False)
-    # fig.axes.get_yaxis().set_visible(False)
-    # plt.savefig('logo.png', dpi=60, bbox_inches='tight', pad_inches=0)
-    # create a small logo consisting of only the "h"
+
+    # big logo
     width, height = 300, 110
     DPI = 100
-    width_inch, height_inch = width / 100, height / 100
+    width_inch, height_inch = width / DPI, height / DPI
     fig = Figure(frameon=False)
     fig.set_size_inches((width_inch, height_inch))
     FigureCanvas(fig)
     ax = fig.add_axes([0., 0., 1., 1.])
     ax.set_axis_off()
     ax.pcolor(data[::-1], edgecolors='#777777', linewidths=0.6)
-    extent = (ax.get_window_extent().
-              transformed(fig.dpi_scale_trans.inverted()))
+    # extent = (ax.get_window_extent().
+    #           transformed(fig.dpi_scale_trans.inverted()))
+    extent = mpl.transforms.Bbox(((0, 0), (width_inch, height_inch)))
     fig.savefig("logo.png", dpi=DPI, transparent=True, bbox_inches=extent,
                 format="png")
     fig.clf()  # important to release memory!
 
     # create a small logo consisting of only the "h"
     width, height = 95, 150 
-    width_inch, height_inch = width / 100, height / 100
+    width_inch, height_inch = width / DPI, height / DPI
     fig = Figure(frameon=False)
     fig.set_size_inches((width_inch, height_inch))
     FigureCanvas(fig)
     ax = fig.add_axes([0., 0., 1., 1.])
     ax.set_axis_off()
     ax.pcolor(data[::-1][1:, 1:9], edgecolors='#888888', linewidths=0.4)
-    extent = (ax.get_window_extent().
-              transformed(fig.dpi_scale_trans.inverted()))
+    extent = mpl.transforms.Bbox(((0, 0), (width_inch, height_inch)))
     fig.savefig("logo_small.png", dpi=DPI, transparent=True, bbox_inches=extent,
                 format="png")
     fig.clf()  # important to release memory!
